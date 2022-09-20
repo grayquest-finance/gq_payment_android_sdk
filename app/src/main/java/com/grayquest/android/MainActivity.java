bfd445a8e7d4175dc966949f55128567e996e530
Avinash <avinash.soni@grayquest.com>
2022-09-20 17:45:56 +0530

    Version Change

--- app/src/main/java/com/grayquest/android/MainActivity.java
+++ app/src/main/java/com/grayquest/android/MainActivity.java
@@ -287,14 +287,21 @@ public class MainActivity extends AppCompatActivity implements GQPaymentSDKListe
         btnPrefill.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                edtClientId.setText("354598fd-575a-4c1f-a6e3-e08fc5ea06d3");
+                // Test Credentials
+                /*edtClientId.setText("354598fd-575a-4c1f-a6e3-e08fc5ea06d3");
                 edtSecretKey.setText("2eb349732594fe777be079fb3c7e557194f55a14");
-                edtGQApi.setText("9db4fc333d8bcf7fee98804105d9fc0c85199d77");
+                edtGQApi.setText("9db4fc333d8bcf7fee98804105d9fc0c85199d77");*/
+
+                // Live Credentials
+                edtClientId.setText("5dcd22af-13b3-4970-9293-55c3be282f93");
+                edtSecretKey.setText("a8aa18f2e043e1313685d0324f38e8599f7c1fed");
+                edtGQApi.setText("122eab04ddd7ad9df25bb77de7494a48afafddee");
+
                 edtStudentID.setText("std_1212");
                 edtFeeEditable.setChecked(false);
                 radioTest.setChecked(true);
                 radioLive.setChecked(false);
-                edtCustomerNumber.setText("8425960199");
+                edtCustomerNumber.setText("8425960118");
                 edtFeeAmount.setText("96000");
                 edtPayableAmount.setText("9600");
             }
