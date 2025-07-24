7569afce8f15d11a4d454a31bd2dcf967732f7b7
1410avi <avinash.soni@grayquest.com>
2025-07-24 20:20:19 +0530

    Token Based SDK initiate - tokenCheckout

--- paymentSDK/src/main/java/com/grayquest/android/payment/sdk/ResponseData.java
+++ paymentSDK/src/main/java/com/grayquest/android/payment/sdk/ResponseData.java
@@ -6,7 +6,9 @@ public class ResponseData {
     @SerializedName("customer_id")
     int customer_id;
     @SerializedName("customer_code")
-String customer_code;
+    String customer_code;
+    @SerializedName("session_code")
+    String session_code;
 
     public int getCustomer_id() {
         return customer_id;
@@ -14,5 +16,9 @@ String customer_code;
 
     public String getCustomer_code() {
         return customer_code;
+    }
+
+    public String getSession_code() {
+        return session_code;
     }
 }
